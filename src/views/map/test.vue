5338d7ecf27f5497b3fbc3258f4c264bdbcb35de
Ori71e2 <Ori71e2@gmail.com>
2019-02-05 11:51:00 +0800

    添加自动生成表单的vue插件

--- src/views/map/test.vue
+++ src/views/map/test.vue
@@ -1,74 +1,148 @@
 <template>
-  <div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="活动名称">
-        <el-input v-model="form.name"/>
-      </el-form-item>
-      <el-form-item label="活动区域">
-        <el-select v-model="form.region" placeholder="请选择活动区域">
-          <el-option label="区域一" value="shanghai"/>
-          <el-option label="区域二" value="beijing"/>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="选择日期" style="width: 100%;"/>
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="选择时间" style="width: 100%;"/>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="即时配送">
-        <el-switch v-model="form.delivery"/>
-      </el-form-item>
-      <el-form-item label="活动性质">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"/>
-          <el-checkbox label="地推活动" name="type"/>
-          <el-checkbox label="线下主题活动" name="type"/>
-          <el-checkbox label="单纯品牌曝光" name="type"/>
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="特殊资源">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="线上品牌商赞助"/>
-          <el-radio label="线下场地免费"/>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="活动形式">
-        <el-input v-model="form.desc" type="textarea"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
-        <el-button>取消</el-button>
-      </el-form-item>
-    </el-form>
+  <div class="panel-body">
+    <form>
+      <vue-form-generator :schema="schema" :model="model" :options="formOptions"/>
+    </form>
   </div>
 </template>
 <script>
+import Vue from 'vue'
+import VueFormGenerator from 'vue-form-generator'
+import 'vue-form-generator/dist/vfg.css'
+
+Vue.use(VueFormGenerator)
+
+/* optional custom validators treated as 'built-in'
+Vue.use(VueFormGenerator, {
+  validators: objectWithCustomValidatorFunctions
+});
+*/
+
 export default {
   data() {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+
+      model: {
+        id: 1,
+        name: 'John Doe',
+        position: '',
+        skills: ['Javascript', 'VueJS'],
+        type: 'SIC',
+        company: '',
+        contactOne: '',
+        phoneOne: '',
+        contactTwo: '',
+        phoneTwo: '',
+        status: true
+      },
+
+      schema: {
+        groups: [
+          {
+            legend: 'Marker Info',
+            fields: [
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'ID (disabled text field)',
+                model: 'id',
+                readonly: true,
+                disabled: true
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Name',
+                model: 'name',
+                // id: 'user_name',
+                placeholder: 'Marker name',
+                // featured: true,
+                required: true
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Type',
+                model: 'type',
+                placeholder: 'Marker\'s Type'
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Position',
+                model: 'position',
+                required: true
+              }
+            ]
+          },
+          {
+            legend: 'Details',
+            fields: [
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Company',
+                model: 'company',
+                placeholder: 'Company'
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Contact One',
+                model: 'contactOne',
+                placeholder: 'Contact One'
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Phone One',
+                model: 'phoneOne',
+                placeholder: 'Phone One'
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Contact Two',
+                model: 'contactTwo',
+                placeholder: 'Contact Two'
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Phone One',
+                model: 'phoneOne',
+                placeholder: 'Phone One'
+              },
+              {
+                type: 'input',
+                inputType: 'text',
+                label: 'Device Owner',
+                model: 'deviceOwner',
+                placeholder: 'Device Owner'
+              },
+              {
+                type: 'select',
+                label: 'Skills',
+                model: 'skills',
+                values: ['Javascript', 'VueJS', 'CSS3', 'HTML5']
+              },
+              {
+                type: 'checkbox',
+                label: 'Status',
+                model: 'status',
+                default: true
+              }
+            ]
+          }
+        ]
+      },
+
+      formOptions: {
+        validateAfterLoad: false,
+        validateAfterChanged: true,
+        validateAsync: true
       }
-    }
-  },
-  methods: {
-    onSubmit() {
-      console.log('submit!')
     }
   }
 }
 </script>
-<style scoped>
-
-</style>
