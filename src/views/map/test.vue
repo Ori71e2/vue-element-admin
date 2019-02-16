27a62720ff3370e873c9dadaf74c60dcb7f15b89
Ori71e2 <Ori71e2@gmail.com>
2019-02-16 10:19:15 +0800

    添加3d效果

--- src/views/map/test.vue
+++ src/views/map/test.vue
@@ -1,256 +1,76 @@
 <template>
   <div class="container">
-    <el-form ref="form" :model="form" :rules="rules" :label-position="labelPosition" label-width="130px">
-      <el-form-item label="Marke ID">
-        <el-tag>{{ form.id }}</el-tag>
-      </el-form-item>
-      <el-form-item label="Marker Name" prop="name">
-        <el-input v-model="form.name"/>
-      </el-form-item>
-      <el-form-item label="Voltage Level" prop="voltageLevel">
-        <el-select v-model="form.voltageLevel" placeholder="Select Voltage Level">
-          <el-option label="220kV" value="220kV"/>
-          <el-option label="110kV" value="110kV"/>
-          <el-option label="35kV" value="35kV"/>
-          <el-option label="" value=""/>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Marker Type" prop="typeCode">
-        <el-button class="open-dialog-button"><i class="el-icon-menu el-icon-menu--custom" @click="openDialog"/>{{ form.typeCodeCnName }}</el-button>
-        <el-dialog :visible.sync="dialogVisible" width="60%" append-to-body @close="closeDialog">
-          <span slot="title"><i class="el-icon-info"/> Icon</span>
-          <div class="flexWrapper jumpButton">
-            <el-button v-for="[mapkey, typeList] of markerTypeMap.entries()" :key="mapkey" type="primary" @click="jump(mapkey)">
-              {{ typeList[0].thirdClass }}
-            </el-button>
-          </div>
-          <div class="dialog-container scroll">
-            <div v-for="[mapkey, typeList] of markerTypeMap.entries()" :key="mapkey" :ref="mapkey" :id="mapkey" class="flexWrapper">
-              <el-button v-for="(type, index) in typeList" :key="index" class="el-button--custom" @click="form.typeCode = type.code; form.typeCodeCnName = type.cnName">
-                <svg-icon :icon-class="type.enName" class="icon-class--custom"/>
-                <p>{{ type.thirdClass }}: {{ type.cnName }}</p>
-              </el-button>
-            </div>
-          </div>
-          <span slot="footer" class="dialog-footer">
-            <el-button @click="cancelSelect">Cancel</el-button>
-            <el-button type="primary" @click="confirmSelect">Confirm</el-button>
-          </span>
-        </el-dialog>
-      </el-form-item>
-      <el-form-item label="Marker Position">
-        <el-input v-model="form.position"/>
-      </el-form-item>
-      <el-form-item label="Belongs To Line" prop="line">
-        <el-input v-model="form.line"/>
-      </el-form-item>
-      <el-form-item label="Tower One" prop="towerOne">
-        <el-input v-model="form.towerOne"/>
-      </el-form-item>
-      <el-form-item label="Tower Two" prop="towerTwo">
-        <el-input v-model="form.towerTwo"/>
-      </el-form-item>
-      <el-form-item label="Company">
-        <el-input v-model="form.company"/>
-      </el-form-item>
-      <el-form-item label="Contact One">
-        <el-input v-model="form.contactOne"/>
-      </el-form-item>
-      <el-form-item label="Phone One">
-        <el-input v-model="form.phoneOne"/>
-      </el-form-item>
-      <el-form-item label="Contact Two">
-        <el-input v-model="form.contactTwo"/>
-      </el-form-item>
-      <el-form-item label="Phone Two">
-        <el-input v-model="form.phoneTwo"/>
-      </el-form-item>
-      <el-form-item label="Device Owner" prop="deviceOwner">
-        <el-input v-model="form.deviceOwner"/>
-      </el-form-item>
-      <el-form-item label="Description" prop="description">
-        <el-input v-model="form.description"/>
-      </el-form-item>
-      <el-form-item label="Remark">
-        <el-input v-model="form.remark"/>
-      </el-form-item>
-      <el-form-item label="FindTime">
-        <el-col :span="10">
-          <el-form-item prop="findTime">
-            <el-date-picker v-model="form.findTime" type="datetime" placeholder="Select Time" style="width: 100%;"/>
-          </el-form-item>
-        </el-col>
-        <el-col :span="4" class="line"><span style="padding-left: 60px; font-weight: bold;">WriteOffTime</span></el-col>
-        <el-col :span="10">
-          <el-form-item prop="writeoffTime">
-            <el-date-picker v-model="form.writeOffTime" type="datetime" placeholder="Select Time" style="width: 100%;"/>
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit('form')">Submit</el-button>
-        <el-button @click="resetForm('form')">Reset</el-button>
-      </el-form-item>
-    </el-form>
+    <swiper ref="swiperTop" :options="swiperOption" class="swiper-inner">
+      <swiper-slide v-for="(value, key) in picture" :key="key">
+        <el-card class="box-card">
+          <div class="slide-content">Slide {{ value }}</div>
+        </el-card>
+      </swiper-slide>
+      <div slot="pagination" class="swiper-pagination"/>
+      <div slot="button-prev" class="swiper-button-prev"/>
+      <div slot="button-next" class="swiper-button-next"/>
+    </swiper>
   </div>
 </template>
+
 <script>
-import SICToSvgName from './components/transmission-line/util/SICToSvgName'
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
 export default {
+  components: {
+    swiper,
+    swiperSlide
+  },
   data() {
     return {
-      form: {
-        id: 1,
-        name: '',
-        voltageLevel: '',
-        typeCode: '',
-        typeCodeCnName: '',
-        position: '',
-        line: '',
-        towerOne: '',
-        towerTwo: '',
-        compant: '',
-        contactOne: '',
-        phoneOne: '',
-        contactTwo: '',
-        phoneTwo: '',
-        deviceOwner: '',
-        findTime: '',
-        writeOffTime: '',
-        description: '',
-        remark: ''
-      },
-      rules: {
-        name: [
-          { required: true, message: 'Please Input Marker Name', trigger: 'blur' },
-          { min: 3, max: 50, message: 'Length 3-50', trigger: 'blur' }
-        ],
-        voltageLevel: [
-          { required: true, message: 'Please Select Voltage Level', trigger: 'change' }
-        ],
-        typeCode: [
-          { required: true, message: 'Please Select Marker Type', trigger: 'change' }
-        ],
-        line: [
-          { required: true, message: 'Please Select Line', trigger: 'blur' }
-        ],
-        towerOne: [
-          { required: true, message: 'Please Select Tower', trigger: 'blur' }
-        ],
-        towerTwo: [
-          { required: true, message: 'Please Select Tower', trigger: 'blur' }
-        ],
-        deviceOwner: [
-          { required: true, message: 'Please Select DeviceOwner', trigger: 'blur' }
-        ],
-        findTime: [
-          { type: 'date', required: true, message: 'Please Select Time', trigger: 'change' }
-        ],
-        type: [
-          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
-        ],
-        description: [
-          { required: true, message: 'Please Write Marker Details', trigger: 'blur' }
-        ]
-      },
-      labelPosition: 'right',
-      dialogVisible: false,
-      sICToSvgName: new SICToSvgName()
-    }
-  },
-  computed: {
-    markerTypeMap() {
-      // console.log(this.sICToSvgName.getCodeListMap().values())
-      return this.sICToSvgName.getCodeListMap()
-    },
-    markerData() {
-      return JSON.parse(this.$store.state.amap.markerData)
-    }
-  },
-  methods: {
-    onSubmit(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          // upLoadMarkerData()
-          // 成功就同步变化
-          const tempForm = JSON.stringify(this.form)
-          this.$store.dispatch('markerData', tempForm)
-          return true
-        } else {
-          console.log('error submit!!')
-          return false
+      picture: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      swiperOption: {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 20,
+          stretch: 20,
+          depth: 300,
+          modifier: 1,
+          slideShadows: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+          el: '.swiper-pagination'
         }
-      })
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
-    },
-    cancelSelect() {
-    },
-    confirmSelect() {
-      this.dialogVisible = false
-    },
-    openDialog() {
-      this.dialogVisible = true
-    },
-    closeDialog() {
-    },
-    jump(id) {
-      // document.getElementById(id).scrollIntoView()
-      this.$refs[id][0].scrollIntoView()
+      }
     }
+  },
+  mounted() {
   }
 }
 </script>
+
 <style scoped>
 .container {
-  width: 100%;
-  padding: 4px;
-}
-.dialog-container {
-  width: 100%;
-}
-.open-dialog-button {
-  width: 100%;
-  text-align: left;
-}
-.el-icon-menu--custom {
-  margin-right: 10px;
-}
-.jumpButton {
-  margin-left: 5px;
-  margin-bottom: 20px;
-}
-.flexWrapper {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-}
-.el-button--custom {
-  width: 150px;
-  height: 120px;
+  width: 90%;
+  padding: 5px;
   margin: 5px;
 }
-.icon-class--custom {
-  width: 60%;
-  height: 75%;
+.swiper-inner {
+  width: 100%;
+  height: 300px;
+  padding-top: 10px;
+  padding-bottom: 10px;
 }
-.scroll {
-  height: 600px;
-  overflow: auto;
+.swiper-slide {
+  background-position: center;
+  background-size: cover;
+  width: 280px;
+  height: 280px;
 }
-.scroll::-webkit-scrollbar {/*滚动条整体样式*/
-  width: 7px;     /*高宽分别对应横竖滚动条的尺寸*/
-  height: 1px;
-}
-.scroll::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
-  border-radius: 10px;
-  box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
-  background: #929191;
-}
-.scroll::-webkit-scrollbar-track {/*滚动条里面轨道*/
-  box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
-  border-radius: 10px;
-  background: #EDEDED;
+.box-card {
+  width: 100%;
+  height: 100%;
 }
 </style>
