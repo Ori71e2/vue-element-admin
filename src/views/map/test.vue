515a270a306e95d5d3224897b34d96a1d294ebc8
Ori71e2 <Ori71e2@gmail.com>
2019-02-16 18:19:42 +0800

    加入虚拟domm

--- src/views/map/test.vue
+++ src/views/map/test.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <swiper ref="swiperTop" :options="swiperOption" class="swiper-inner">
-      <swiper-slide v-for="(value, key) in picture" :key="key">
+      <!-- <swiper-slide v-for="(value, key) in picture" :key="key">
         <el-card class="box-card">
           <div class="slide-content">Slide {{ value }}</div>
         </el-card>
-      </swiper-slide>
+      </swiper-slide> -->
       <div slot="pagination" class="swiper-pagination"/>
       <div slot="button-prev" class="swiper-button-prev"/>
       <div slot="button-next" class="swiper-button-next"/>
@@ -26,13 +26,19 @@ export default {
       picture: [1, 2, 3, 4, 5, 6, 7, 8, 9],
       swiperOption: {
         effect: 'coverflow',
-        grabCursor: true,
+        // grabCursor: true,
         centeredSlides: true,
-        slidesPerView: 'auto',
+        // 虚拟slide必须确定数字
+        slidesPerView: 5,
+        // rotate：slide做3d旋转时Y轴的旋转角度。默认50。
+        // stretch：每个slide之间的拉伸值，越大slide靠得越紧。 默认0。
+        // depth：slide的位置深度。值越大z轴距离越远，看起来越小。 默认100。
+        // modifier：depth和rotate和stretch的倍率，相当于depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
+        // slideShadows：开启slide阴影。默认 true。
         coverflowEffect: {
-          rotate: 20,
-          stretch: 20,
-          depth: 300,
+          rotate: 10,
+          stretch: 0,
+          depth: 200,
           modifier: 1,
           slideShadows: true
         },
@@ -41,7 +47,21 @@ export default {
           prevEl: '.swiper-button-prev'
         },
         pagination: {
-          el: '.swiper-pagination'
+          el: '.swiper-pagination',
+          type: 'fraction'
+        },
+        virtual: {
+          slides: (function() {
+            const slides = []
+            for (let i = 0; i < 600; i += 1) {
+              slides.push('Slide ' + (i + 1))
+            }
+            return slides
+          }()),
+          renderSlide: function(slide, index) {
+            // <div class="swiper-slide"></div> 这个结构必须包含，不能缺少
+            return '<div class="swiper-slide"">索引是' + index + '+内容是内容是内容是内容是内容是内容是内容是' + slide + '</div>'
+          }
         }
       }
     }
@@ -64,9 +84,9 @@ export default {
   padding-bottom: 10px;
 }
 .swiper-slide {
-  background-position: center;
-  background-size: cover;
-  width: 280px;
+  /* background-position: center;
+  background-size: cover; */
+  width: 300px !important;
   height: 280px;
 }
 .box-card {
